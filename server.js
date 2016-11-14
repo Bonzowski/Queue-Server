c96bad664c73e402b9a7ff60a9da4a123ba5fa48
Marcin Kozlowski <kozlowski.marcin2@gmail.com>
2016-11-14 23:36:56 +0100

    Update server.js
    no object assign

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 var express = require('express'),
     fs      = require('fs'),
     app     = express();
-    
-Object.assign=require('object-assign')
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
