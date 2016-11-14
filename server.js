f05ccf5e5f73a2f03157c01533502aa33a074b33
Marcin Kozlowski <kozlowski.marcin2@gmail.com>
2016-11-14 23:33:44 +0100

    Update server.js
    removed morgan

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 //  OpenShift sample Node application
 var express = require('express'),
     fs      = require('fs'),
-    app     = express(),
-    morgan  = require('morgan');
+    app     = express();
     
 Object.assign=require('object-assign')
-app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
