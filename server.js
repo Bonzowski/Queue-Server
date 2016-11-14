ed38e20a4f4218211fa735de1c0599835251ab6a
Bonzowski <kozlowski.marcin2@gmail.com>
2016-11-14 22:00:44 +0100

    server from stack1

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-var Queue = require("./queue")
-var q1 = new Queue(1);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-var QueueManager = require("./queueManager");
-var queueManeger = new QueueManager(5); 
-
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+server.listen(server_port, server_ip_address, function(){
+ console.log("Listening on " + server_ip_address 
+           + ", server_port " + server_port);
 });
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+var Queue = require("./queue")
+var q1 = new Queue(1);
+
+var QueueManager = require("./queueManager");
+var queueManeger = new QueueManager(5); 
 
 app.get('/getTicket', function (req, res) {
 	
