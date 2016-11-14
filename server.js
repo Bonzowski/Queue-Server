3c938b34b178bd6b7cca61d9c8a502ba966ac429
Bonzowski <kozlowski.marcin2@gmail.com>
2016-11-14 22:15:32 +0100

    server from stack2

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-var http = require('http');
-var express = require('express');
-var app = express();
+//  OpenShift sample Node application
+var express = require('express'),
+    fs      = require('fs'),
+    app     = express(),
+    eps     = require('ejs'),
+    morgan  = require('morgan');
+    
+Object.assign=require('object-assign')
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
 
-server.listen(server_port, server_ip_address, function(){
- console.log("Listening on " + server_ip_address 
-           + ", server_port " + server_port);
-});
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -47,3 +50,8 @@ app.get('/queues', function(req,res) {
 
 	res.json({Queues: queueManeger.queues});
 });
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
